041ae4cb54e07ccd2b25779674df23c65cebe2b7
needleinahaystack3107 <90634986+needleinahaystack3107@users.noreply.github.com>
2022-10-11 15:50:00 -0400

    Added pipeline and imported datasets

---
+++ enzymechallenge/src/enzymechallenge/pipelines/data_processing_and_feature_engineering/__init__.py
@@ -0,0 +1,10 @@
+"""
+This is a boilerplate pipeline 'data_processing_and_feature_engineering'
+generated using Kedro 0.18.3
+"""
+
+from .pipeline import create_pipeline
+
+__all__ = ["create_pipeline"]
+
+__version__ = "0.1"
---
+++ enzymechallenge/src/enzymechallenge/pipelines/data_processing_and_feature_engineering/nodes.py
@@ -0,0 +1,4 @@
+"""
+This is a boilerplate pipeline 'data_processing_and_feature_engineering'
+generated using Kedro 0.18.3
+"""
